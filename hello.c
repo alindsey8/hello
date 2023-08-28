e1f459e45cc39b7dffba59cd52c673f91f658750
Adam <alindsey8@huskers.unl.edu>
2023-08-28 14:54:55 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Adam Lindsey
+ * 2023/08/28
+ *
+ * A simple program that prints my name.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Adam Lindsey\n");
+
+    return 0;
+
+}
